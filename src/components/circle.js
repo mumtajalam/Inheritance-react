24275ee9ea9d044005d022421d6e15800022d51c
Md Mumtaj Alam <98735031+mumtajalam@users.noreply.github.com>
2022-07-07 23:39:43 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,10 @@
 // import using require
+require(./Shape.js);
 
+class Circle extends Shape{
+  this.calculateArea = function(){}
+}
 // declare class
-
+export Circle;
 
 // export class using module.exports
