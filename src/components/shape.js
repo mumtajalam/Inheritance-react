8b3b3938a1129ec237d87ee80c9fa3adc2bb9dee
Md Mumtaj Alam <98735031+mumtajalam@users.noreply.github.com>
2022-07-07 23:32:13 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,8 @@
 // declare class
-
+class Shape{
+  this.drawShape = function(){}
+    this.calculateArea = function(){}
+      this.color = 'red';
+}
+export Shape;
 // export class using module.exports
